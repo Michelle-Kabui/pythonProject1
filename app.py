3acc95f61fce99f64ef83784ae1fd83ab856cf1c
Michelle-Kabui <michelle.kabui@strathmore.edu>
2024-04-27 23:04:08 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/home')
+def home():
+    server_id = os.environ.get('SERVER_ID', 'Unknown')
+    return f"Hello from Server: {server_id}"
+
+
+# Endpoint for /heartbeat
+@app.route('/heartbeat', methods=['GET'])
+def heartbeat():
+    return '', 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
